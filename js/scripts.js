63e48ffb18da018d141aef335f3f7c9b23e2d163
Hennadii <gennadiy.chickunov@gmail.com>
2021-04-21 14:52:13 +0300

    Оптимизация изображений, скриптов + подключение focusVisible библиотеки
    добавил возомжность изменять цену пиццы при клике на размер и так же это все меняется в форме заказа,
     сжал изображения для оптимизации, скорости загрузки картинок, скрипты js все переместил в 1 файл?, ранее это делалось для удобства процесса работы в начале проекта, так же переместил normalize в main css, чтобы не было лишнего запроса. также подключил библиотеку focusVisible это решение для людей с ограниченными возможностями , в main css есть строчка кода .js-focus-visible :focus:not(.focus-visible) {
      outline: none;
    } которая отвечает за , при клике у нас нет outline, но при табе у нас это свойство работает ... работа уже на финальной стадии. следующий билд будет завершающий

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-// данный скрипт это надежное меню по юзабилити и удобству пользователя
-$(document).ready(function () {
-	$(".header-page__hamburger").click(function (event) {
-		$(".header-page__hamburger, .popup").toggleClass("active");
-		// Отмена скролла при включенном мобильном меню
-		$("body").toggleClass("lock");
-	});
-});
